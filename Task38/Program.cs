7a72be83c41d700a9b818e13026acd5ddcf8627b
Alexandr <k9108@mail.ru>
2023-04-24 21:53:03 +0300

    Add a new tascs

---
+++ Task38/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int number_from_user = UserCreateArray("Введите число ");
+
+int UserCreateArray(string numbers)
+{
+    System.Console.Write(numbers);
+    int i;
+    while(!int.TryParse(System.Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число");
+    }
+    return i;
+}
+
+void FillRandom(int[] numbers)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < numbers.Length; i++)
+    numbers[i] = rnd.Next(1, 9);
+}
+
+
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.WriteLine(numbers[i]);
+    }
+    System.Console.WriteLine();
+}
+
+
+void FillArray ()
+{
+    int[] array = new int [number_from_user];
+    FillRandom(array);
+    PrintArray(array);
+}
+
+FillArray();
+
+
